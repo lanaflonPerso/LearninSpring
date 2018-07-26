94dc5a4c4116677c3a51a058e8a2297c7eb69848
master <olimpik>
2018-07-26 14:49:30 +0300

    Spring Rest API

---
+++ SpringRestDemo/src/main/java/ru/ivmiit/service/models/Token.java
@@ -0,0 +1,26 @@
+package ru.ivmiit.service.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.ivmiit.service.models.User;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String value;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
---
+++ SpringRestDemo/src/main/java/ru/ivmiit/service/services/UsersService.java
@@ -0,0 +1,12 @@
+package ru.ivmiit.service.services;
+
+import ru.ivmiit.service.forms.UserForm;
+import ru.ivmiit.service.models.User;
+
+import java.util.List;
+
+public interface UsersService {
+    void signUp(UserForm userForm);
+
+    List<User> findAll();
+}
